40e1403649d531e0f12f86c98ea61493855c3f7b
Md Samirul Islam <mdsamirulislam725@gmail.com>
2024-05-17 16:39:08 +0530

    Fix port issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./startup/db')()
 require('./startup/validation')()
 
 // PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   winston.info(`Server is listening on port ${PORT}`)
 })
